195f013d9cd657fee575a6711a4fd78692a26fb6
Mayurr9 <mayurranpise1234@gmail.com>
2021-09-22 13:20:10 +0530

    initial

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const path = require('path');
+const expresslayouts = require('express-ejs-layouts');
+const PORT = process.env.PORT || 3300;
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+// set template engine
+app.use(expresslayouts)
+app.set('views', path.join(__dirname,'/resources/views'))
+app.set('view engine', 'ejs');
+
+app.listen(PORT , () => {
+    console.log(`listening on port ${PORT}`);
+})
