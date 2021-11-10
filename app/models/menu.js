ca16c41978fa4439fda1959236dbab722689164a
Mayurr9 <mayurranpise1234@gmail.com>
2021-11-10 14:12:51 +0530

    Menu Updated

--- app/models/menu.js
+++ app/models/menu.js
@@ -5,7 +5,7 @@ const menuSchema = new Schema({
     name: { type: String, required: true},
     image: { type: String, required: true},
     price: { type: Number, required: true},
-    size: { type: String, required: true}
+    isVeg: { type: String, required: true}
 })
 
 module.exports = mongoose.model('Menu', menuSchema)
