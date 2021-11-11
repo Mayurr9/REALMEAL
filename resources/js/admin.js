11553c763376d751ddf94fb5197f026553bd40d5
Mayurr9 <mayurranpise1234@gmail.com>
2021-11-11 17:22:28 +0530

    bug removed

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -43,10 +43,9 @@ export function initAdmin(socket) {
                             <input type="hidden" name="orderId" value="${ order._id }">
                             <select name="status" onchange="this.form.submit()"
                                 class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-                                <option value="order_placed"
-                                    ${ order.status === 'order_placed' ? 'selected' : '' }>
+                                <option value="order_placed"  ${ order.status === 'order_placed' ? 'selected' : '' }>
                                     Placed</option>
-                                    <option value="confirmed" ${ order.status === 'confirmed' ? 'selected' : '' }>
+                                <option value="confirmed" ${ order.status === 'confirmed' ? 'selected' : '' }>
                                     Confirmed</option>
                                 <option value="prepared" ${ order.status === 'prepared' ? 'selected' : '' }>
                                     Prepared</option>
